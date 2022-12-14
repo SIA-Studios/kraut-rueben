38d1aacb7ec1a0107afe06e2f71cebbe1fc5a23a
thedvxl <tim@urbutt.de>
2022-12-14 09:01:50 +0100

    dfsdfds

--- lib/utils/table.dart
+++ lib/utils/table.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kraut_rueben/utils/transitions.dart';
 
-Widget databaseTableEntry(String text, BuildContext context, int recipeId, String queryType) {
+Widget databaseTableEntry(String text, BuildContext context) {
   return GestureDetector(
     onTap: () {
       showInputPopup(title: "test", initialValue: text);
