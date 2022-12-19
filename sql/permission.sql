47b8c29cab3829f70c0851e4f0043ae69cf57083
Finn <dereinefinn@gmail.com>
2022-12-19 19:33:19 +0100

    add permission script

--- sql/permission.sql
+++ sql/permission.sql
@@ -1,4 +1,46 @@
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
 
-GRANT SELECT ON krautundrueben.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON krautundrueben.* TO 'user'@'%';
+/******************************************************************************/
+/***              Lieferant Rolle (Rechte auf Kunden-View)                  ***/
+/******************************************************************************/
+
+CREATE ROLE lieferant;
+GRANT SELECT ON krautundrueben.LIEFERUNG_ALLE_KUNDEN TO lieferant;
+
+/******************************************************************************/
+/***              Praktikanten Rolle (Select Zugriff)                       ***/
+/******************************************************************************/
+
+CREATE ROLE praktikant;
+GRANT SELECT ON krautundrueben.* TO praktikant;
+
+/******************************************************************************/
+/***         Mitarbeiter Rolle (Select, Update & Insert Zugriff)            ***/
+/******************************************************************************/
+
+CREATE ROLE mitarbeiter;
+GRANT SELECT ON krautundrueben.* TO mitarbeiter;
+GRANT UPDATE ON krautundrueben.* TO mitarbeiter;
+GRANT INSERT ON krautundrueben.* TO mitarbeiter;
+
+/******************************************************************************/
+/***                    Administrator Rolle (Vollzugriff)                   ***/
+/******************************************************************************/
+
+CREATE ROLE administrator;
+GRANT ALL PRIVILEGES ON krautundrueben.* TO administrator;
+
+/******************************************************************************/
+/***                            Benutzer Erstellung                         ***/
+/******************************************************************************/
+
+CREATE USER 'tom'@'%';
+GRANT lieferant TO 'tom'@'%';
+
+CREATE USER 'till'@'%';
+GRANT praktikant TO 'till'@'%';
+
+CREATE USER 'tim'@'%';
+GRANT mitarbeiter TO 'tim'@'%';
+
+CREATE USER 'finn'@'%';
+GRANT administrator TO 'finn'@'%';
